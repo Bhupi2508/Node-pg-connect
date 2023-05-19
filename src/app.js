04cc30a49caf9df26034752fc122708efb6cae47
Bhupi2508 <bhupendrasigh.ec18@gmail.com>
2023-05-19 22:25:13 +0530

    data added through sql query

--- src/app.js
+++ src/app.js
@@ -8,20 +8,33 @@ const PORT = process.env.PORT;
 const { pool } = require("./db");
 
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+    res.send("Hello world!");
 });
 
 // async-await pattern for get callback argument
 app.get("/users", async (req, res) => {
-  try {
-    // pool.query runs a query on the first available idle client and
-    // return its result
-    const result = await pool.query(`Select * from users`);
-    res.send(result.rows);
-  } catch (err) {
-    console.error('Error executing query', err.stack)
-    res.status(500).json({ error: 'Something went wrong'});
-  }
+    try {
+        // pool.query runs a query on the first available idle client and
+        // return its result
+        const result = await pool.query(`Select * from users`);
+        res.send(result.rows);
+    } catch (err) {
+        console.error('Error executing query', err.stack)
+        res.status(500).json({ error: 'Something went wrong' });
+    }
+});
+
+// async-await pattern for get callback argument
+app.get("/addUsers", async (req, res) => {
+    try {
+        // add user if doesn't exist
+        const result = await pool.query(`INSERT INTO users (name, email)
+    VALUES ('Bhupi', 'bhupi@gmail.com');`);
+        res.send(result.rows);
+    } catch (err) {
+        console.error('Error executing query', err.stack)
+        res.status(500).json({ error: 'Something went wrong' });
+    }
 });
 
 app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}/`));
